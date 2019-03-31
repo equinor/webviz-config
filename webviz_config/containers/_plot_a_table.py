614a31facb2083af8851e3da12bfc2ca45d30889
Hans Kallekleiv <lathans@protonmail.com>
2019-03-31 16:34:54 +0200

    Made plot options more generic. Cleanup

--- webviz_config/containers/_plot_a_table.py
+++ webviz_config/containers/_plot_a_table.py
@@ -1,6 +1,7 @@
 import inspect
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 from pathlib import Path
 from uuid import uuid4
 import dash_html_components as html
@@ -14,40 +15,18 @@ import plotly_express as px
 class PlotATable:
 
     def __init__(self, app, title: str, csv_file: Path,
-                 plot: str = 'scatter', x: str = None, y: str = None,
-                 z: str = None, size: str = None, color: str = None):
+                 plot_options: dict = {}):
 
         self.title = title
-
-        self.button_id = 'submit-button-{}'.format(uuid4())
-        self.div_id = 'output-state-{}'.format(uuid4())
+        self.plot_options = plot_options
         self.graph_id = 'graph-id{}'.format(uuid4())
-        self.graph_selector = 'graph-selector{}'.format(uuid4())
         self.csv_file = csv_file
         self.data = get_data(self.csv_file)
         self.columns = list(self.data.columns)
         self.numeric_columns = list(
             self.data.select_dtypes(include=[np.number]).columns)
-
-        self.plot = plot
-        self.x = x if x else self.columns[0]
-        self.y = y if y else self.columns[0]
-        self.z = z if z else self.columns[0]
-        self.size = size
-        self.color = color
-
         self.selector_row = 'selector-row{}'.format(uuid4())
-        self.x_selector = 'x-selector{}'.format(uuid4())
-        self.x_div_selector = 'x-div_selector{}'.format(uuid4())
-        self.y_selector = 'y-selector{}'.format(uuid4())
-        self.y_div_selector = 'y-div-selector{}'.format(uuid4())
-        self.z_selector = 'z-selector{}'.format(uuid4())
-        self.z_div_selector = 'z-div-selector{}'.format(uuid4())
-        self.size_selector = 'size-selector{}'.format(uuid4())
-        self.size_div_selector = 'size-div-selector{}'.format(uuid4())
-        self.color_selector = 'size-selector{}'.format(uuid4())
-        self.color_div_selector = 'size-div-selector{}'.format(uuid4())
-
+        self.plot_option_divs = 'plot-option-divs{}'.format(uuid4())
         self.set_callbacks(app)
 
     def add_webvizstore(self):
@@ -55,162 +34,211 @@ class PlotATable:
 
     @property
     def plots(self):
+        '''A list of available plots and their options'''
         return {
-            'scatter': 'scatter',
-            'scatter_3d': 'scatter_3d',
+            'scatter': ['x', 'y', 'size', 'color', 'facet_col'],
+            'histogram': ['x', 'y', 'color', 'facet_col', 'barmode',
+                          'barnorm', 'histnorm'],
+            'bar': ['x', 'y', 'color', 'facet_col'],
+            'scatter_3d': ['x', 'y', 'z', 'size', 'color'],
+            'line': ['x', 'y', 'color', 'line_group', 'facet_col'],
+            'line_3d': ['x', 'y', 'z', 'size', 'color', 'facet_col'],
+            'box': ['x', 'y', 'color', 'facet_col'],
+            'violin': ['x', 'y', 'color', 'facet_col'],
+            'scatter_matrix': ['size', 'color'],
+            'parallel_coordinates': [],
+            'parallel_categories': [],
+            'density_contour': ['x', 'y', 'size', 'color', 'facet_col'],
             # 'scatter_polar':'scatter_polar',
             # 'scatter_ternary':'scatter_ternary',
             # 'scatter_mapbox':'scatter_mapbox',
             # 'scatter_geo':'scatter_geo',
-            'line': 'line',
-            'line_3d': 'line_3d',
             # 'line_polar':'line_polar',
             # 'line_ternary':'line_ternary',
             # 'line_mapbox':'line_mapbox',
             # 'line_geo':'line_geo',
-            'bar': 'bar',
             # 'bar_polar':'bar_polar',
-            'violin': 'violin',
-            'box': 'box',
-            'histogram': 'histogram',
-            'scatter_matrix': 'scatter_matrix',
-            'parallel_coordinates': 'parallel_coordinates',
-            'parallel_categories': 'parallel_categories',
             # 'choropleth':'choropleth',
-            'density_contour': 'density_contour'
         }
 
     @property
-    def style_plot_options(self):
-        '''Simple grid layout for the selector row'''
+    def plot_args(self):
+        '''A list of possible plot options and their default values'''
+        return OrderedDict(
+            {
+                'x': {
+                    'options': self.columns,
+                    'value': self.plot_options.get('x', self.columns[0])
+                },
+                'y': {
+                    'options': self.columns,
+                    'value': self.plot_options.get('y', self.columns[0])
+                },
+                'z': {
+                    'options': self.columns,
+                    'value': self.plot_options.get('z', self.columns[0])
+                },
+                'size': {
+                    'options': self.numeric_columns,
+                    'value': self.plot_options.get('size', None)
+                },
+                'color': {
+                    'options': self.columns,
+                    'value': self.plot_options.get('color', None)
+                },
+                'facet_col': {
+                    'options': self.columns,
+                    'value': self.plot_options.get('facet_col', None)
+                },
+                'line_group': {
+                    'options': self.columns,
+                    'value': self.plot_options.get('line_group', None)
+                },
+                'barmode': {
+                    'options': ['stack', 'group', 'overlay', 'relative'],
+                    'value': self.plot_options.get('barmode', 'stack')
+                },
+                'barnorm': {
+                    'options': ['fraction', 'percent'],
+                    'value': self.plot_options.get('barnorm', None)
+                },
+                'histnorm': {
+                    'options': ['percent', 'propability', 'density',
+                                'propability density'],
+                    'value': self.plot_options.get('histnorm', None)
+                }
+
+            })
+
+    def plot_option_layout(self):
+        '''Renders a dropdown widget for each plot option'''
+        divs = []
+        # The plot type dropdown is handled separate
+        divs.append(
+            html.Div(
+                style=self.style_options_div,
+                children=[
+                    html.P('Plot type'),
+                    dcc.Dropdown(
+                        id='{}-plottype'.format(self.plot_option_divs),
+                        clearable=False,
+                        options=[{'label': i, 'value': i}
+                                 for i in self.plots.keys()],
+                        value=self.plot_options.get('type', 'scatter')
+                    )
+                ]
+            )
+        )
+        # Looping through all available plot options
+        # and renders a dropdown widget
+        for key, arg in self.plot_args.items():
+            divs.append(
+                html.Div(
+                    style=self.style_options_div,
+                    id='{}-div-{}'.format(self.plot_option_divs, key),
+                    children=[
+                        html.P(key),
+                        dcc.Dropdown(
+                            id='{}-{}'.format(self.plot_option_divs, key),
+                            clearable=False,
+                            options=[{'label': i, 'value': i}
+                                     for i in arg['options']],
+                            value=arg['value'])
+                    ])
+            )
+        return divs
+
+    @property
+    def style_options_div(self):
+        '''Style for active plot options'''
+        return {
+            'display': 'grid',
+        }
+
+    @property
+    def style_options_div_hidden(self):
+        '''Style for hidden plot options'''
+        return {
+            'display': 'none',
+        }
+
+    @property
+    def style_page_layout(self):
+        '''Simple grid layout for the page'''
         return {
             'display': 'grid',
             'align-content': 'space-around',
             'justify-content': 'space-between',
-            'grid-template-columns': 'repeat(6, 1fr)'
+            'grid-template-columns': '1fr 5fr'
         }
 
     @property
     def layout(self):
-        return html.Div([
+        return html.Div(children=[
             html.H1(self.title),
-            html.Div(
-                id=self.selector_row,
-                style=self.style_plot_options,
-                children=[
-                    html.Div(children=[
-                             html.P('Select plot type'),
-                             dcc.Dropdown(
-                                 id=self.graph_selector,
-                                 clearable=False,
-                                 options=[{'label': i, 'value': i}
-                                          for i in self.plots.keys()],
-                                 value=self.plot)
-                             ]),
-                    html.Div(id=self.x_div_selector, children=[
-                             html.P('X column'),
-                             dcc.Dropdown(
-                                 id=self.x_selector,
-                                 clearable=False,
-                                 options=[{'label': i, 'value': i}
-                                          for i in self.columns],
-                                 value=self.x)
-                             ]),
-                    html.Div(id=self.y_div_selector, children=[
-                             html.P('Y column'),
-                             dcc.Dropdown(
-                                 id=self.y_selector,
-                                 clearable=False,
-                                 options=[{'label': i, 'value': i}
-                                          for i in self.columns],
-                                 value=self.y)
-                             ]),
-                    html.Div(id=self.z_div_selector, children=[
-                             html.P('Z column'),
-                             dcc.Dropdown(
-                                 id=self.z_selector,
-                                 clearable=False,
-                                 options=[{'label': i, 'value': i}
-                                          for i in self.columns],
-                                 value=self.z)
-                             ]),
-                    html.Div(id=self.size_div_selector, children=[
-                             html.P('Size column'),
-                             dcc.Dropdown(
-                                 id=self.size_selector,
-                                 clearable=True,
-                                 options=[{'label': i, 'value': i}
-                                          for i in self.numeric_columns],
-                                 value=self.size)
-                             ]),
-                    html.Div(id=self.color_div_selector, children=[
-                        html.P('Color column'),
-                        dcc.Dropdown(
-                            id=self.color_selector,
-                            clearable=True,
-                            options=[{'label': i, 'value': i}
-                                     for i in self.columns],
-                            value=self.color)
-                    ])
-                ]),
-            dcc.Graph(id=self.graph_id)
+            html.Div(style=self.style_page_layout, children=[
+                html.Div(
+                    id=self.selector_row,
+                    children=self.plot_option_layout()),
+                dcc.Graph(id=self.graph_id)
+            ])
         ])
+
+    @property
+    def plot_output_callbacks(self):
+        '''Creates list of output dependencies for callback
+        The outputs are the graph, and the style of the plot options'''
+        outputs = []
+        outputs.append(
+            Output(self.graph_id, 'figure')
+        )
+        for plot_arg in self.plot_args.keys():
+            outputs.append(
+                Output(
+                    '{}-div-{}'.format(self.plot_option_divs, plot_arg),
+                    'style'
+                )
+            )
+        return outputs
+
+    @property
+    def plot_input_callbacks(self):
+        '''Creates list of input dependencies for callback
+        The inputs are the plot type and the current value
+        for each plot option
+        '''
+        inputs = []
+        inputs.append(
+            Input(
+                '{}-plottype'.format(self.plot_option_divs),
+                'value')
+        )
+        for plot_arg in self.plot_args.keys():
+            inputs.append(
+                Input(
+                    '{}-{}'.format(self.plot_option_divs, plot_arg),
+                    'value')
+            )
+        return inputs
 
     def set_callbacks(self, app):
         @app.callback(
-            [
-                Output(self.graph_id, 'figure'),
-                Output(self.x_div_selector, 'style'),
-                Output(self.y_div_selector, 'style'),
-                Output(self.z_div_selector, 'style'),
-                Output(self.size_div_selector, 'style'),
-                Output(self.color_div_selector, 'style')
-            ],
-            [
-                Input(self.graph_selector, 'value'),
-                Input(self.x_selector, 'value'),
-                Input(self.y_selector, 'value'),
-                Input(self.z_selector, 'value'),
-                Input(self.size_selector, 'value'),
-                Input(self.color_selector, 'value')
-            ])
-        def update_output(plot_type, x, y, z, size, color):
-            plotfunc = getattr(px._chart_types, self.plots[plot_type])
-            plotargs = inspect.getargspec(plotfunc)._asdict()
-            args = {}
-            if 'x' in plotargs['args']:
-                args['x'] = x
-                x_style = {'display': 'block'}
-            else:
-                x_style = {'display': 'none'}
-            if 'y' in plotargs['args']:
-                args['y'] = y
-                y_style = {'display': 'block'}
-            else:
-                y_style = {'display': 'none'}
-            if 'z' in plotargs['args']:
-                args['z'] = z
-                z_style = {'display': 'block'}
-            else:
-                z_style = {'display': 'none'}
-            if 'size' in plotargs['args']:
-                args['size'] = size
-                size_style = {'display': 'block'}
-            else:
-                size_style = {'display': 'none'}
-            if 'color' in plotargs['args']:
-                args['color'] = color
-                color_style = {'display': 'block'}
-            else:
-                color_style = {'display': 'none'}
-            return (
-                plotfunc(self.data, **args),
-                x_style,
-                y_style,
-                z_style,
-                size_style,
-                color_style
-            )
+            self.plot_output_callbacks,
+            self.plot_input_callbacks
+        )
+        def update_output(*args):
+            '''Updates the graph and shows/hides plot options'''
+            plot_type = args[0]
+            plotfunc = getattr(px._chart_types, plot_type)
+            plotargs = {}
+            div_style = []
+            for name, plot_arg in zip(self.plot_args.keys(), args[1:]):
+                if name in self.plots[plot_type]:
+                    plotargs[name] = plot_arg
+                    div_style.append(self.style_options_div)
+                else:
+                    div_style.append(self.style_options_div_hidden)
+            return (plotfunc(self.data, **plotargs), *div_style)
 
 
 @cache.memoize(timeout=cache.TIMEOUT)
