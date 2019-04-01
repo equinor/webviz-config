148a6bcc81033c9058a27dad3cfa82c6dd0cc349
Hans Kallekleiv <lathans@protonmail.com>
2019-04-01 10:20:11 +0200

    Added option to lock plot settings. Cleanup

--- webviz_config/containers/_plot_a_table.py
+++ webviz_config/containers/_plot_a_table.py
@@ -15,18 +15,19 @@ import plotly_express as px
 class PlotATable:
 
     def __init__(self, app, title: str, csv_file: Path,
-                 plot_options: dict = {}):
+                 plot_options: dict = {}, lock: bool = False):
 
         self.title = title
         self.plot_options = plot_options
         self.graph_id = 'graph-id{}'.format(uuid4())
+        self.lock = lock
         self.csv_file = csv_file
         self.data = get_data(self.csv_file)
         self.columns = list(self.data.columns)
         self.numeric_columns = list(
             self.data.select_dtypes(include=[np.number]).columns)
         self.selector_row = 'selector-row{}'.format(uuid4())
-        self.plot_option_divs = 'plot-option-divs{}'.format(uuid4())
+        self.plot_option_id = 'plot-option{}'.format(uuid4())
         self.set_callbacks(app)
 
     def add_webvizstore(self):
@@ -42,23 +43,13 @@ class PlotATable:
             'bar': ['x', 'y', 'color', 'facet_col'],
             'scatter_3d': ['x', 'y', 'z', 'size', 'color'],
             'line': ['x', 'y', 'color', 'line_group', 'facet_col'],
-            'line_3d': ['x', 'y', 'z', 'size', 'color', 'facet_col'],
+            'line_3d': ['x', 'y', 'z', 'color'],
             'box': ['x', 'y', 'color', 'facet_col'],
             'violin': ['x', 'y', 'color', 'facet_col'],
             'scatter_matrix': ['size', 'color'],
             'parallel_coordinates': [],
             'parallel_categories': [],
-            'density_contour': ['x', 'y', 'size', 'color', 'facet_col'],
-            # 'scatter_polar':'scatter_polar',
-            # 'scatter_ternary':'scatter_ternary',
-            # 'scatter_mapbox':'scatter_mapbox',
-            # 'scatter_geo':'scatter_geo',
-            # 'line_polar':'line_polar',
-            # 'line_ternary':'line_ternary',
-            # 'line_mapbox':'line_mapbox',
-            # 'line_geo':'line_geo',
-            # 'bar_polar':'bar_polar',
-            # 'choropleth':'choropleth',
+            'density_contour': ['x', 'y', 'color', 'facet_col'],
         }
 
     @property
@@ -106,7 +97,11 @@ class PlotATable:
                     'options': ['percent', 'propability', 'density',
                                 'propability density'],
                     'value': self.plot_options.get('histnorm', None)
-                }
+                },
+                'trendline': {
+                    'options': self.numeric_columns,                                
+                    'value': None
+                },
 
             })
 
@@ -120,7 +115,7 @@ class PlotATable:
                 children=[
                     html.P('Plot type'),
                     dcc.Dropdown(
-                        id='{}-plottype'.format(self.plot_option_divs),
+                        id='{}-plottype'.format(self.plot_option_id),
                         clearable=False,
                         options=[{'label': i, 'value': i}
                                  for i in self.plots.keys()],
@@ -135,11 +130,11 @@ class PlotATable:
             divs.append(
                 html.Div(
                     style=self.style_options_div,
-                    id='{}-div-{}'.format(self.plot_option_divs, key),
+                    id='{}-div-{}'.format(self.plot_option_id, key),
                     children=[
                         html.P(key),
                         dcc.Dropdown(
-                            id='{}-{}'.format(self.plot_option_divs, key),
+                            id='{}-{}'.format(self.plot_option_id, key),
                             clearable=False,
                             options=[{'label': i, 'value': i}
                                      for i in arg['options']],
@@ -165,13 +160,22 @@ class PlotATable:
     @property
     def style_page_layout(self):
         '''Simple grid layout for the page'''
-        return {
-            'display': 'grid',
-            'align-content': 'space-around',
-            'justify-content': 'space-between',
-            'grid-template-columns': '1fr 5fr'
-        }
+        if not self.lock:
+            return {
+                'display': 'grid',
+                'align-content': 'space-around',
+                'justify-content': 'space-between',
+                'grid-template-columns': '1fr 5fr'
+            }
+        return {}
 
+    @property
+    def style_selectors(self):
+        if self.lock:
+            return {'display': 'none'}
+        else:
+            return {}
+    
     @property
     def layout(self):
         return html.Div(children=[
@@ -179,6 +183,7 @@ class PlotATable:
             html.Div(style=self.style_page_layout, children=[
                 html.Div(
                     id=self.selector_row,
+                    style=self.style_selectors,
                     children=self.plot_option_layout()),
                 dcc.Graph(id=self.graph_id)
             ])
@@ -195,7 +200,7 @@ class PlotATable:
         for plot_arg in self.plot_args.keys():
             outputs.append(
                 Output(
-                    '{}-div-{}'.format(self.plot_option_divs, plot_arg),
+                    '{}-div-{}'.format(self.plot_option_id, plot_arg),
                     'style'
                 )
             )
@@ -210,13 +215,13 @@ class PlotATable:
         inputs = []
         inputs.append(
             Input(
-                '{}-plottype'.format(self.plot_option_divs),
+                '{}-plottype'.format(self.plot_option_id),
                 'value')
         )
         for plot_arg in self.plot_args.keys():
             inputs.append(
                 Input(
-                    '{}-{}'.format(self.plot_option_divs, plot_arg),
+                    '{}-{}'.format(self.plot_option_id, plot_arg),
                     'value')
             )
         return inputs
@@ -244,4 +249,4 @@ class PlotATable:
 @cache.memoize(timeout=cache.TIMEOUT)
 @webvizstore
 def get_data(csv_file) -> pd.DataFrame:
-    return pd.read_csv(csv_file)
+    return pd.read_csv(csv_file, index_col=None)
