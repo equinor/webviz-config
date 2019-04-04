1d328ca8b972d23b5d34d3e7906b8b6e6c6d59e4
Anders Fredrik Kiær <akia@statoil.com>
2019-04-04 14:41:29 +0200

    WIP: Add automatic pypi deploy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webviz-config',
-    version='0.1',
+    version='0.0.1',
     description='Configuration file support for webviz',
     url='https://github.com/Statoil/webviz-config',
     author='R&T Equinor',
