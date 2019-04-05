34a8ee9a152458c626d7c1b20523ad155975b062
Hans Kallekleiv <hakal@statoil.com>
2019-04-05 13:08:06 +0200

    Fixed formatting

--- webviz_config/containers/_table_plotter.py
+++ webviz_config/containers/_table_plotter.py
@@ -23,23 +23,24 @@ a database.
 * `csv_file`: Path to the csv file containing the tabular data. Either absolute
               path or relative to the configuration file.
 * `plot_options`: A dictionary of plot options to initialize the plot with
-* `lock`: If `True`, Only the plot is shown, all dropdowns for changing
+* `lock`: If `True`, only the plot is shown, all dropdowns for changing
           plot options are hidden.
 '''
+
     def __init__(self, app, title: str, csv_file: Path,
                  plot_options: dict = {}, lock: bool = False):
 
         self.title = title
         self.plot_options = plot_options
-        self.graph_id = 'graph-id{}'.format(uuid4())
+        self.graph_id = f'graph-id{uuid4()}'
         self.lock = lock
         self.csv_file = csv_file
         self.data = get_data(self.csv_file)
         self.columns = list(self.data.columns)
         self.numeric_columns = list(
             self.data.select_dtypes(include=[np.number]).columns)
-        self.selector_row = 'selector-row{}'.format(uuid4())
-        self.plot_option_id = 'plot-option{}'.format(uuid4())
+        self.selector_row = f'selector-row{uuid4()}'
+        self.plot_option_id = f'plot-option{uuid4()}'
         self.set_callbacks(app)
 
     def add_webvizstore(self):
@@ -127,7 +128,7 @@ a database.
                 children=[
                     html.P('Plot type'),
                     dcc.Dropdown(
-                        id='{}-plottype'.format(self.plot_option_id),
+                        id=f'{self.plot_option_id}-plottype',
                         clearable=False,
                         options=[{'label': i, 'value': i}
                                  for i in self.plots.keys()],
@@ -142,11 +143,11 @@ a database.
             divs.append(
                 html.Div(
                     style=self.style_options_div,
-                    id='{}-div-{}'.format(self.plot_option_id, key),
+                    id=f'{self.plot_option_id}-div-{key}',
                     children=[
                         html.P(key),
                         dcc.Dropdown(
-                            id='{}-{}'.format(self.plot_option_id, key),
+                            id=f'{self.plot_option_id}-{key}',
                             clearable=False,
                             options=[{'label': i, 'value': i}
                                      for i in arg['options']],
@@ -172,21 +173,16 @@ a database.
     @property
     def style_page_layout(self):
         '''Simple grid layout for the page'''
-        if not self.lock:
-            return {
-                'display': 'grid',
-                'align-content': 'space-around',
-                'justify-content': 'space-between',
-                'grid-template-columns': '1fr 5fr'
-            }
-        return {}
+        return {} if self.lock else {
+            'display': 'grid',
+            'align-content': 'space-around',
+            'justify-content': 'space-between',
+            'grid-template-columns': '1fr 5fr'
+        }
 
     @property
     def style_selectors(self):
-        if self.lock:
-            return {'display': 'none'}
-        else:
-            return {}
+        return {'display': 'none'} if self.lock else {}
 
     @property
     def layout(self):
@@ -211,10 +207,7 @@ a database.
         )
         for plot_arg in self.plot_args.keys():
             outputs.append(
-                Output(
-                    '{}-div-{}'.format(self.plot_option_id, plot_arg),
-                    'style'
-                )
+                Output(f'{self.plot_option_id}-{plot_arg}', 'style')
             )
         return outputs
 
@@ -226,16 +219,9 @@ a database.
         '''
         inputs = []
         inputs.append(
-            Input(
-                '{}-plottype'.format(self.plot_option_id),
-                'value')
-        )
+            Input(f'{self.plot_option_id}-plottype', 'value'))
         for plot_arg in self.plot_args.keys():
-            inputs.append(
-                Input(
-                    '{}-{}'.format(self.plot_option_id, plot_arg),
-                    'value')
-            )
+            inputs.append(Input(f'{self.plot_option_id}-{plot_arg}', 'value'))
         return inputs
 
     def set_callbacks(self, app):
