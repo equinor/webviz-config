2ebc80581f9a653df2e8711f9d770d576349abd5
Anders Fredrik Kiær <akia@statoil.com>
2019-05-16 11:03:14 +0200

    Change from px to vh

--- webviz_config/containers/_embed_pdf.py
+++ webviz_config/containers/_embed_pdf.py
@@ -10,18 +10,25 @@ This container embeds a given PDF file into the page.
 
 * `pdf_file`: Path to the PDF file to include. Either absolute path or
   relative to the configuration file.
-* `height`: Height of the PDF object (in pixels).
+* `height`: Height of the PDF object (in percent of viewport height).
 * `width`: Width of the PDF object (in percent of available space).
+
+_Note_: Webviz does not scan your PDF for malicious code. Make sure it comes from a trusted source.
 '''
 
-    def __init__(self, pdf_file: Path, height: int = 600, width: int = 100):
+    def __init__(self, pdf_file: Path, height: int = 80, width: int = 100):
         self.pdf_url = webviz_assets.add(pdf_file)
         self.height = height
         self.width = width
 
     @property
     def layout(self):
+
+        style = {
+                 'height': f'{self.height}vh',
+                 'width': f'{self.width}%'
+                }
+
         return html.Embed(src=self.pdf_url,
-                          width=f'{self.width}%',
-                          height=f'{self.height}px',
+                          style=style,
                           type='application/pdf')
