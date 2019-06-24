a38d821fcb2e3f948ed74a9aaeee049dcc1b1c32
Hans Kallekleiv <hakal@statoil.com>
2019-06-24 15:59:23 +0200

    Mocking webvizstore in test

--- tests/test_syntax_highlighter.py
+++ tests/test_syntax_highlighter.py
@@ -1,6 +1,12 @@
+import mock
+from pathlib import Path
 import dash
 from webviz_config.common_cache import cache
 from webviz_config.containers import _syntax_highlighter
+
+# mocked functions
+get_path = 'webviz_config.containers'\
+                '._syntax_highlighter.get_path'
 
 
 def test_syntax_highlighter(dash_duo):
@@ -10,8 +16,10 @@ def test_syntax_highlighter(dash_duo):
     app.scripts.config.serve_locally = True
     app.config.suppress_callback_exceptions = True
     cache.init_app(app.server)
-    code_file = './tests/data/basic_example.yaml'
-    page = _syntax_highlighter.SyntaxHighlighter(app, code_file)
-    app.layout = page.layout
-    dash_duo.start_server(app)
-    assert dash_br.get_logs() == [], "browser console should contain no error"
+    code_file = Path('./tests/data/basic_example.yaml')
+    with mock.patch(get_path) as mock_path:
+        mock_path.return_value = code_file
+        page = _syntax_highlighter.SyntaxHighlighter(app, code_file)
+        app.layout = page.layout
+        dash_duo.start_server(app)
+        assert dash_duo.get_logs() == [], "browser console should contain no error"
