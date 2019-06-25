7b812455fbff62794e18176b85c0bbb626370673
Hans Kallekleiv <hakal@statoil.com>
2019-06-25 10:52:48 +0200

    Update DataTable container to use latest Dash Table (#57)
    * Use new dash table syntax
    
    * Updated example
    
    * Added data table render test
    
    * Remove unused import
    
    * small rewrite of variable names
    
    * small rewrite of variable names

---
+++ tests/test_data_table.py
@@ -0,0 +1,42 @@
+import mock
+import pandas as pd
+import dash
+from webviz_config.common_cache import cache
+from webviz_config.containers import _data_table
+
+# mocked functions
+get_data = 'webviz_config.containers'\
+    '._data_table.get_data'
+
+
+def test_data_table(dash_duo):
+
+    app = dash.Dash(__name__)
+    app.css.config.serve_locally = True
+    app.scripts.config.serve_locally = True
+    app.config.suppress_callback_exceptions = True
+    cache.init_app(app.server)
+    code_file = './tests/data/example_data.csv'
+    with mock.patch(get_data) as mock_path:
+        mock_path.return_value = pd.read_csv(code_file)
+        page = _data_table.DataTable(code_file)
+        app.layout = page.layout
+        dash_duo.start_server(app)
+        assert dash_duo.get_logs() == [], "browser console should contain no error"
+
+
+def test_data_table_with_settings(dash_duo):
+
+    app = dash.Dash(__name__)
+    app.css.config.serve_locally = True
+    app.scripts.config.serve_locally = True
+    app.config.suppress_callback_exceptions = True
+    cache.init_app(app.server)
+    code_file = './tests/data/example_data.csv'
+    with mock.patch(get_data) as mock_path:
+        mock_path.return_value = pd.read_csv(code_file)
+        page = _data_table.DataTable(csv_file=code_file,  sorting=False,
+                                     filtering=False, pagination=False)
+        app.layout = page.layout
+        dash_duo.start_server(app)
+        assert dash_duo.get_logs() == [], "browser console should contain no error"
