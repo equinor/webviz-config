a2747fe842e2d3bad4aad5c78eb40aac4e7de37a
Hans Kallekleiv <hakal@statoil.com>
2019-08-22 18:09:05 +0200

    Use pytest tmp dir

--- tests/test_portable.py
+++ tests/test_portable.py
@@ -3,22 +3,24 @@ import sys
 import subprocess
 import dash
 
-def test_portable(dash_duo):
-    #Build a portable webviz from config file
-    subprocess.call(['webviz', 'build', 'basic_example.yaml', '--portable', 'testapp'], cwd='examples')
-    fn = path.dirname(__file__) + '/../examples/testapp/webviz_app.py'
 
-    #Remove Talisman
+def test_portable(dash_duo, tmpdir):
+    # Build a portable webviz from config file
+    appdir = path.join(tmpdir, 'app')
+    subprocess.call(['webviz', 'build', 'basic_example.yaml',
+                     '--portable', appdir], cwd='examples')
+    # Remove Talisman
+    fn = path.join(appdir, 'webviz_app.py')
     with open(fn, "r") as f:
         lines = f.readlines()
     with open(fn, "w") as f:
         for line in lines:
             if not line.strip("\n").startswith("Talisman"):
                 f.write(line)
-    sys.path.append(path.dirname(__file__) + '/../examples/testapp')
-
-    #Import and test portable app
+    # Import generated app
+    sys.path.append(appdir)
     import webviz_app
+    # Build and test app
     dash_app = dash.Dash(__name__, external_stylesheets=[])
     dash_app = webviz_app.app
     dash_duo.start_server(dash_app)
@@ -26,4 +28,3 @@ def test_portable(dash_duo):
                  'syntax_highlighting_example', 'plot_a_table', 'last_page']:
         dash_duo.wait_for_element(f'#{page}').click()
     assert dash_duo.get_logs() == [], 'browser console should contain no error'
-    subprocess.call(['rm', '-rf', 'testapp'], cwd='examples')
