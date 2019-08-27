b86d2a73667123a5a645438221e862a7b31de27c
Hans Kallekleiv <lathans@protonmail.com>
2019-08-27 21:46:16 +0200

    Added data filtering to TablePlotter

--- tests/test_table_plotter.py
+++ tests/test_table_plotter.py
@@ -20,6 +20,8 @@ def test_table_plotter(dash_duo):
 
     # Checking that no plot options are defined
     assert page.plot_options == {}
+    # Check that filter is not active
+    assert page.use_filter == False
 
     # Checking that the selectors are not hidden
     selector_row = dash_duo.find_element(f'#{page.selector_row}')
@@ -43,6 +45,46 @@ def test_table_plotter(dash_duo):
         plot_option_dd = dash_duo.find_element(f'#{page.plot_option_id}-{option}')
         assert plot_option_dd.text == 'Well'
 
+def test_table_plotter_filter(dash_duo):
+
+    app = dash.Dash(__name__)
+    app.config.suppress_callback_exceptions = True
+    cache.init_app(app.server)
+
+    csv_file = './tests/data/example_data.csv'
+    page = _table_plotter.TablePlotter(app, csv_file, filter_cols=['Well'])
+    app.layout = page.layout
+    dash_duo.start_server(app)
+
+    # Wait for the app to render(there is probably a better way...)
+    time.sleep(5)
+
+    # Checking that no plot options are defined
+    assert page.plot_options == {}
+    # Check that filter is active
+    assert page.use_filter == True
+    assert page.filter_cols == ['Well']
+    # Checking that the selectors are not hidden
+    selector_row = dash_duo.find_element(f'#{page.selector_row}')
+    assert selector_row.get_attribute('style') == ''
+
+    # Checking that the correct plot type is initialized
+    plot_dd = dash_duo.find_element(f'#{page.plot_option_id}-plottype')
+    assert plot_dd.text == 'scatter'
+
+    # Checking that only the relevant options are shown
+    for plot_option in page.plot_args.keys():
+        plot_option_dd = dash_duo.find_element(
+            f'#{page.plot_option_id}-div-{plot_option}')
+        if plot_option not in page.plots['scatter']:
+            assert plot_option_dd.get_attribute('style') == 'display: none;'
+        else:
+            assert plot_option_dd.get_attribute('style') == 'display: grid;'
+
+    # Checking that options are initialized correctly
+    for option in ['x', 'y']:
+        plot_option_dd = dash_duo.find_element(f'#{page.plot_option_id}-{option}')
+        assert plot_option_dd.text == 'Well'
 
 def test_initialized_table_plotter(dash_duo):
 
