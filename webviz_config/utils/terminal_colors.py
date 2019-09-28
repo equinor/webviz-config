d2a8ef002e20e29ab202cd14e424a16138842641
Anders Fredrik Kiær <akia@statoil.com>
2019-09-28 08:50:22 +0200

    Remove unnecessary line break

--- webviz_config/utils/terminal_colors.py
+++ webviz_config/utils/terminal_colors.py
@@ -1,9 +1,8 @@
-RED = "\x1b[5;37;41m"
-GREEN = "\x1b[5;37;42m"
 BLUE = "\x1b[5;37;44m"
-YELLOW = "\x1b[2;30;43m"
-
+GREEN = "\x1b[5;37;42m"
 PURPLE = "\x1b[0;37;45m"
+RED = "\x1b[5;37;41m"
+YELLOW = "\x1b[2;30;43m"
 
 BOLD = "\x1b[1m"
 END = "\x1b[0m"
