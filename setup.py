86a33de8ec47b84550c90657c188a7e27446e4c6
Anders Fredrik Kiær <akia@statoil.com>
2019-10-03 14:33:16 +0200

    Bump wcc + add test

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "pandas~=0.24",
         "pyarrow~=0.11",
         "pyyaml~=5.1",
-        "webviz-core-components>=0.0.8",
+        "webviz-core-components>=0.0.10",
     ],
     tests_require=TESTS_REQUIRES,
     extras_require={"tests": TESTS_REQUIRES},
