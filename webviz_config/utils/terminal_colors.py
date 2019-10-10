9debdb1574c63020e23107f31397e2eca36ae130
Anders Kiær <akia@statoil.com>
2019-10-10 06:30:09 +0000

    Remove blinking text

--- webviz_config/utils/terminal_colors.py
+++ webviz_config/utils/terminal_colors.py
@@ -1,7 +1,7 @@
-BLUE = "\x1b[5;37;44m"
-GREEN = "\x1b[5;37;42m"
-PURPLE = "\x1b[0;37;45m"
-RED = "\x1b[5;37;41m"
+BLUE = "\x1b[37;44m"
+GREEN = "\x1b[37;42m"
+PURPLE = "\x1b[37;45m"
+RED = "\x1b[37;41m"
 YELLOW = "\x1b[2;30;43m"
 
 BOLD = "\x1b[1m"
