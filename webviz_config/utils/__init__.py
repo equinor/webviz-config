20e0811d83290ca6cae5c4017e23dcbeb3b22d2b
Hans Kallekleiv <hakal@statoil.com>
2020-01-30 14:35:36 +0100

    Function to calculate rounded step values for RangeSlider (#186)

--- webviz_config/utils/__init__.py
+++ webviz_config/utils/__init__.py
@@ -1,2 +1,3 @@
 from ._available_port import get_available_port
 from ._silence_flask_startup import silence_flask_startup
+from ._dash_component_utils import calculate_slider_step
---
+++ webviz_config/utils/_dash_component_utils.py
@@ -0,0 +1,16 @@
+import math
+
+
+def calculate_slider_step(min_value: float, max_value: float, steps=100):
+    """Calculates a step value for use in e.g. dcc.RangeSlider() component
+    that will always be rounded.
+
+    The number of steps will be atleast the number
+    of input steps, but might not be precisely the same due to use of the floor function.
+
+    This function is necessary since there is currently no precision control in the underlying
+    React component (https://github.com/react-component/slider/issues/275).
+
+    """
+
+    return 10 ** math.floor(math.log10((max_value - min_value) / steps))
