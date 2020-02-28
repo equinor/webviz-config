b05851c8a21ff61d3b9990c2b88a59fdbf419233
Asgeir Nyvoll <asny@equinor.com>
2020-02-28 10:58:17 +0100

    Webvizstore default value error example

--- webviz_config/plugins/_example_portable.py
+++ webviz_config/plugins/_example_portable.py
@@ -12,22 +12,32 @@ class ExamplePortable(WebvizPluginABC):
         super().__init__()
 
         self.some_number = some_number
+        self.some_string = "a"
 
     def add_webvizstore(self) -> List[tuple]:
-        return [(input_data_function, [{"some_number": self.some_number}])]
+        return [
+            (
+                input_data_function,
+                [{"some_string": self.some_string, "some_number": self.some_number}],
+            )
+        ]
 
     @property
     def layout(self) -> str:
-        return str(input_data_function(self.some_number))
+        return str(input_data_function(self.some_string, some_number=self.some_number))
 
 
 @CACHE.memoize(timeout=CACHE.TIMEOUT)
 @webvizstore
-def input_data_function(some_number: int) -> pd.DataFrame:
+def input_data_function(
+    some_string: str, some_number: int, some_bool: bool = True
+) -> pd.DataFrame:
     print("This time I'm actually doing the calculation...")
     return pd.DataFrame(
         data={
             "col1": [some_number, some_number * 2],
             "col2": [some_number * 3, some_number * 4],
+            "col3": [some_string, some_string + "b"],
+            "col4": [some_bool, not some_bool],
         }
     )
