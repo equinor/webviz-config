a1d3c7314b2da49ead5272656263c1778a05475e
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-09-21 15:18:27 +0200

    Adjusted advanced config example

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -9,14 +9,14 @@ from webviz_config._docs._create_schema import create_schema
 @pytest.mark.parametrize(
     "config_file_path",
     [
-        (pathlib.Path("examples") / "basic_example.yaml").read_text(),
-        (pathlib.Path("examples") / "basic_example_advanced_menu.yaml").read_text(),
+        (pathlib.Path("examples") / "basic_example.yaml"),
+        (pathlib.Path("examples") / "basic_example_advanced_menu.yaml"),
     ],
 )
-def test_schema(config_file_path: str):
+def test_schema(config_file_path: pathlib.Path):
     """Tests both that the generated schema is valid,
     and that the input configuration is valid according to the schema.
     """
 
-    config = yaml.safe_load(config_file_path)
+    config = yaml.safe_load(config_file_path.read_text())
     jsonschema.validate(instance=config, schema=create_schema())
