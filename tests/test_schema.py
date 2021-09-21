8e2468851e5d0ad39db64e90028dda6e459975c7
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-09-21 14:22:38 +0200

    Added `test_schema` parametrization and test for second basic example

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,17 +1,22 @@
 import pathlib
-
+import pytest
 import yaml
 import jsonschema
 
 from webviz_config._docs._create_schema import create_schema
 
 
-def test_schema():
+@pytest.mark.parametrize(
+    "config_file_path",
+    [
+        (pathlib.Path("examples") / "basic_example.yaml").read_text(),
+        (pathlib.Path("examples") / "basic_example_advanced_menu.yaml").read_text(),
+    ],
+)
+def test_schema(config_file_path: str):
     """Tests both that the generated schema is valid,
     and that the input configuration is valid according to the schema.
     """
 
-    config = yaml.safe_load(
-        (pathlib.Path("examples") / "basic_example.yaml").read_text()
-    )
+    config = yaml.safe_load(config_file_path)
     jsonschema.validate(instance=config, schema=create_schema())
