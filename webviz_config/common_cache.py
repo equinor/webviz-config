2c824e8da1ca095c390546483b4164ee4a56849d
Hans Kallekleiv <HansKallekleiv@users.noreply.github.com>
2022-06-09 10:10:31 +0200

    Add filesystem cache

--- webviz_config/common_cache.py
+++ webviz_config/common_cache.py
@@ -1,5 +1,7 @@
 import flask_caching
 
 
-CACHE = flask_caching.Cache(config={"CACHE_TYPE": "simple"})
+CACHE = flask_caching.Cache(
+    config={"CACHE_TYPE": "FileSystemCache", "CACHE_DIR": "./flaskcaching"}
+)
 CACHE.TIMEOUT = 3600
