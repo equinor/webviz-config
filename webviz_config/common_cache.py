3165d6260dee45457de138d4838847b83739fbc6
Asgeir Nyvoll <asny@equinor.com>
2022-08-25 13:02:17 +0200

    Initilize cache with directly with default timeout of 1 hr

--- webviz_config/common_cache.py
+++ webviz_config/common_cache.py
@@ -1,5 +1,6 @@
 import flask_caching
 
 
-CACHE = flask_caching.Cache(config={"CACHE_TYPE": "simple"})
-CACHE.TIMEOUT = 3600
+CACHE = flask_caching.Cache(
+    config={"CACHE_TYPE": "simple", "DEFAULT_CACHE_TIMEOUT": 3600}
+)
