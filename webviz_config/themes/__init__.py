66403b56dcdce4d53a361f6ff070cfbef03034fb
Asgeir Nyvoll <asny@equinor.com>
2024-01-25 15:25:28 +0100

    support python 3.11 and 3.12

--- webviz_config/themes/__init__.py
+++ webviz_config/themes/__init__.py
@@ -1,3 +1,4 @@
+import sys
 from importlib.metadata import entry_points
 
 from .. import WebvizConfigTheme
@@ -7,11 +8,23 @@ installed_themes = {default_theme.theme_name: default_theme}
 
 __all__ = ["installed_themes"]
 
-for entry_point in entry_points().get("webviz_config_themes", []):
-    theme = entry_point.load()
+if sys.version_info < (3, 10, 0):
+    for entry_point in entry_points().get("webviz_config_themes", []):
+        theme = entry_point.load()
 
-    globals()[entry_point.name] = theme
-    __all__.append(entry_point.name)
+        globals()[entry_point.name] = theme
+        __all__.append(entry_point.name)
 
-    if isinstance(theme, WebvizConfigTheme):
-        installed_themes[theme.theme_name] = theme
+        if isinstance(theme, WebvizConfigTheme):
+            installed_themes[theme.theme_name] = theme
+else:
+    for entry_point in entry_points(  # pylint: disable=unexpected-keyword-arg
+        group="webviz_config_themes"
+    ):
+        theme = entry_point.load()
+
+        globals()[entry_point.name] = theme
+        __all__.append(entry_point.name)
+
+        if isinstance(theme, WebvizConfigTheme):
+            installed_themes[theme.theme_name] = theme
